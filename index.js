b6f2ac4c9cfe010f318f1c4235f2b469cfcd19ab
CollegeDream <78008854+CollegeDream@users.noreply.github.com>
2021-02-09 20:48:32 -0800

    Update index.js

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ client.on("message", async message => {
         isInGuild = true
       }else if(guildID){
         // This will run if the user is not in the guild, but is in any guild.
-        message.member.roles.remove(config.memberRole);
+        
       }
       message.member.setNickname(username).catch(console.log)
       message.member.roles.add([config.verifiedRole]).then(()=>{
