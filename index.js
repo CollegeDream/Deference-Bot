d46ded079769d1a0c3ef2b1c1522701bc35041d6
CollegeDream <78008854+CollegeDream@users.noreply.github.com>
2021-02-12 16:54:15 -0800

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ async function getLinkedDiscord(username){
 }
 
 async function removeMemberRole(user, role){
-    await user.roles.remove(role);
+    await user.roles.remove([role, role, role]);
 }
 
 const Discord = require('discord.js')
