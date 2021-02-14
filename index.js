afd7550ce46c633ef0a03dca987f0436d088b163
CollegeDream <78008854+CollegeDream@users.noreply.github.com>
2021-02-13 22:54:00 -0800

    Update index.js

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ client.on("message", async message => {
       }else if(guildID !== config.hypixelGuild || message.member.roles.cache.has(config.memberRole)){
         // User is not in guild, but has the member role
         message.channel.send(embed_member_left)
-        setTimeout(function(){message.member.roles.remove(config.memberRole)}, 60);
+        setTimeout(function(){message.member.roles.remove(config.memberRole)}, 100);
       }
       
 
