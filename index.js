1a6d2c405b9c0cfffccba6a4298a6a49c0d124e9
CollegeDream <78008854+CollegeDream@users.noreply.github.com>
2021-02-13 16:04:44 -0800

    Update index.js

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ client.on("message", async message => {
         const embed_failed = new Discord.MessageEmbed()
         .setColor('#fc0303')
         .setTitle('Verification failed  ❌')
-        .addField(str3, 'Here is a **[guide](https://www.youtube.com/watch?v=gqUPbkxxKLI&feature=emb_logo)** on how to link your account on Hypixel', false)
+        .addField('"Update your tag from" + `${linkedAccount}` + " to " + `${message.author.tag}`', 'Here is a **[guide](https://www.youtube.com/watch?v=gqUPbkxxKLI&feature=emb_logo)** on how to link your account on Hypixel', false)
        
         //.addField('Looks like your account is not linked or is out-dated', 'Here is a **[guide](https://www.youtube.com/watch?v=gqUPbkxxKLI&feature=emb_logo)** on how to link your account on Hypixel', true)
         .attachFiles(['./images/guide.png'])
