4b3e0f7f35cf9f227cb0e49a468780e943b1479f
CollegeDream <78008854+CollegeDream@users.noreply.github.com>
2021-02-13 23:02:14 -0800

    Update index.js

--- index.js
+++ index.js
@@ -120,16 +120,17 @@ client.on("message", async message => {
       const guild = await guildInfo(guildID).catch(e=>null);
       let guildName;
       
-      if(guildID){
+      if(!guildID){
         // Checks if it is in any guild
         guildName = guild.name;
         embed_guildName.setTitle('Guild found: ' + guildName)
         message.channel.send(embed_guildName)
         setTimeout(function(){message.member.roles.remove(config.memberRole)}, 100);
-      }else if(guildID !== config.hypixelGuild && message.member.roles.cache.has(config.memberRole)){
+      }else if(guildID !== config.hypixelGuild || message.member.roles.cache.has(config.memberRole)){
         // User is not in guild, but has the member role
-        message.channel.send(embed_member_left)
-        setTimeout(function(){message.member.roles.remove(config.memberRole)}, 100);
+        message.channel.send(embed_member_left).
+        then(function(){message.member.roles.remove(config.memberRole)});
+        //setTimeout(function(){message.member.roles.remove(config.memberRole)}, 100);
       }
       
 
@@ -146,7 +147,7 @@ client.on("message", async message => {
         message.channel.send(embed_verified)
        
 
-          if(isInGuild){
+          if(!isInGuild){
             // If it is in the guild, then add the member role
 
             message.channel.send(embed_member_given)
