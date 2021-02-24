a0e452218e63887807f20d447157d1df01b483d4
CollegeDream <78008854+CollegeDream@users.noreply.github.com>
2021-02-23 16:47:02 -0800

    Update index.js

--- index.js
+++ index.js
@@ -51,13 +51,11 @@ const client = new Discord.Client();
 
 client.on("ready", () =>console.log(`${client.user.tag} is online!`))
 
-client.user.setActivity("Guarding the server's gate", {
-  type: "GAMING",
-  url: "https://www.twitch.tv/monstercat"
-});
-
 client.on("message", async message => {
   //if(message.author.bot) return;
+  client.user.setActivity("Guarding the server's gate", {
+  type: "GAMING",
+  });
   if(!message.guild || message.guild.id !== config.targetGuild) return;
     const args = message.content.slice(config.prefix.length).split(/ +/g)
     const command = args.shift()
