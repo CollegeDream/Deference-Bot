2fc139ba8111485c25d22423e87046d4626a9ffc
CollegeDream <78008854+CollegeDream@users.noreply.github.com>
2021-02-23 18:00:18 -0800

    Update index.js

--- index.js
+++ index.js
@@ -49,8 +49,8 @@ const Discord = require('discord.js')
 
 const client = new Discord.Client();
 
-client.on(client.user.setStatus("Watching over the server"), () =>console.log(`${client.user.tag} is online!`))
-
+client.on("ready", () => {client.user.setActivity("Watching over the server")})
+//=>console.log(`${client.user.tag} is online!`)
 client.on("message", async message => {
   //if(message.author.bot) return;
   
