4b3b836c5f4837366f372381ca032150adbcf31f
Minh Nguyen <mnguyen24@my.whitworth.edu>
2021-02-26 13:35:36 -0800

    d

--- index.js
+++ index.js
@@ -184,7 +184,7 @@ client.on("message", async message => {
             embed_verified.addField('Member of Deference', `Given <@&686070737194450995> role`, false)
             message.channel.send(embed_verified)
             message.channel.send(`Display Name: ${player.displayname}`)
-            message.channel.send(`Bedwars level: ${bedwarsLevel}`)
+            message.channel.send(`Bedwars level: ${player.achievements.bedwars_level}`)
             message.channel.send(`Network level : ${networkLevel} (for testing)`)
             message.member.roles.add(config.memberRole).catch(e=>{
               console.log(e)
