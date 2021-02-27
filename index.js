d19379ee0d87aaa5a19ac89be11e867bd1a2f73b
Minh Nguyen <mnguyen24@my.whitworth.edu>
2021-02-26 22:11:14 -0800

    plyaerUUID

--- index.js
+++ index.js
@@ -72,6 +72,9 @@ client.on("message", async message => {
         } else {
           const IGN = args[1];
           const playerUUID = await getUUID(IGN);
+          if(!playerUUID){
+            message.channel.send('Could not find a player with this IGN');
+          } else {
           const player = await getPlayer(IGN);
           message.channel.send('Player\'s past names:\n');
           var myArray = player.knownAliases;
@@ -79,6 +82,7 @@ client.on("message", async message => {
             message.channel.send(`${myArray[i]}\n`);
           }
         }
+      }
       }
       
     }
