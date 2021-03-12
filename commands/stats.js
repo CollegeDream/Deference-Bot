a0074fd1ed853f3c446ddeab031a32f6bcbe3425
Minh Nguyen <mnguyen24@my.whitworth.edu>
2021-03-11 21:09:50 -0800

    ds

--- commands/stats.js
+++ commands/stats.js
@@ -3,63 +3,12 @@ module.exports = {
     name: 'stats',
     description: 'print statistics',
     execute(message, args){
-        const config = require("../config.json")
-        const fetch = require("node-fetch")
-        const fs = require("fs")
+        
 
-        function getUUID(username) {
-            return fetch(`https://api.mojang.com/users/profiles/minecraft/${username}`)
-            .then(data => data.json())
-            .then((player) => {
-            return player.id
-            }).catch(e=>null);
-        }
-        async function getPlayer(username){
-        const id = await getUUID(username)
-        return fetch(`https://api.hypixel.net/player?uuid=${id}&key=${config.apiKey}`)
-        .then(result => result.json())
-        .then(({player}) => {
-        return player
-        }).catch(e=>null);
-        };
-
-        async function getGuild(username){
-        const id = await getUUID(username);
-        return fetch(`https://api.hypixel.net/findGuild?byUuid=${id}&key=${config.apiKey}`)
-        .then(result => result.json())
-        .then(({guild}) => {
-        return guild
-        }).catch(e=>null);
-        };
-
-        async function guildInfo(guildID){
-        return fetch(`https://api.hypixel.net/guild?key=${config.apiKey}&id=${guildID}`)
-        .then(result => result.json())
-        .then(({guild}) => {
-        return guild
-        }).catch(e=>console.log(e));
-        }
-
-
-        async function getLinkedDiscord(username){
-        const user = await getPlayer(username)
-        if(!user || !user.socialMedia || !user.socialMedia.links || !user.socialMedia.links.DISCORD) return null
-        return user.socialMedia.links.DISCORD
-        }
-
-        async function getOnlineStatus(username){
-        return fetch(`https://api.slothpixel.me/api/players/${username}/status`)
-        .then(result => result.json())
-        .then(({game}) => {
-        return game
-        }).catch(e=>console.log(e));
-        };
-        //const args = message.content.slice(config.prefix.length).trim().split(/ +/);
-       //const command = args.shift().toLowerCase();
         const username = args[0]
         if(!username) return message.reply("You need to say your minecraft username.")
   
-        const linkedAccount = getLinkedDiscord(username)
+        const linkedAccount = getLinkedDiscord(username);
         const playerUUID = getUUID(username);
         const authorID = message.author.id;
         const player = getPlayer(username);
@@ -80,6 +29,6 @@ module.exports = {
         message.channel.send(`Online status: ${status.type}\n
         Network level: ${networkLevel.toFixed(2)}\n
         First joined: ${joinedDate}\n
-        )`)
+        Bedwars stars: ${player.achievements.bedwars_level} (this bot is in beta)`)
     },
 };
