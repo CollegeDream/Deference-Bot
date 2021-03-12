e06027efdbb1fee02bdf16d7c60fe4e4398644c0
Minh Nguyen <mnguyen24@my.whitworth.edu>
2021-03-12 11:06:08 -0800

    rank

--- commands/guildInfo.js
+++ commands/guildInfo.js
@@ -50,7 +50,7 @@ module.exports = {
             async function getExpHistory(username){
             const response = await fetch(`https://api.slothpixel.me/api/guilds/${username}`)
             const result = await response.json();
-            let exp = result.members.exp_history;
+            let exp = result.members.rank;
             message.channel.send(exp);
             }
             console.log('hey')
