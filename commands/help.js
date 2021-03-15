24261650b347479eb2cce88fe6487839208cc877
Minh Nguyen <mnguyen24@my.whitworth.edu>
2021-03-14 22:57:31 -0700

    &

--- commands/help.js
+++ commands/help.js
@@ -2,6 +2,6 @@ module.exports = {
     name: 'help',
     description: 'send help messages',
     async execute(message, args){
-        message.channel.send('The bot is early in its development process, here are the current commands: \n &verify\n &test aliases\n &hug\n &g\n stats\n ')
+        message.channel.send('The bot is early in its development process, here are the current commands: \n &verify\n &test aliases\n &hug\n &g\n &stats\n ')
     },
 }
