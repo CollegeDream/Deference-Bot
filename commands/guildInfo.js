1e0a6a8a9964354dc8d23dd91a2384a820cacc6f
Minh Nguyen <mnguyen24@my.whitworth.edu>
2021-03-15 23:17:48 -0700

    update

--- commands/guildInfo.js
+++ commands/guildInfo.js
@@ -91,7 +91,7 @@ module.exports = {
 
                             }]
                           },
-
+                        offset: false
                       })
                     
                 }
