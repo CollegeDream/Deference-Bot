34ff5d0c63afa4839f267349abd32d86db04d825
Minh Nguyen <mnguyen24@my.whitworth.edu>
2021-03-17 23:11:30 -0700

    di

--- commands/guildInfo.js
+++ commands/guildInfo.js
@@ -118,9 +118,8 @@ module.exports = {
             guild_Embed.addField(`Total GEXP for the week: ${expTotal}`, '\u200B', false)
             message.channel.send(guild_Embed);
         }
-        client.on("unhandledRejection", (err) => {
-            message.channel.send(err);
-        })
     },  
 }
-
+process.on("unhandledRejection", (err) => {
+    message.channel.send(err);
+})
