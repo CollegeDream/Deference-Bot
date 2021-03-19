2616f0bcdebdd74b1c9360031c5f2b94b452d737
Minh Nguyen <mnguyen24@my.whitworth.edu>
2021-03-18 23:39:46 -0700

    remote mongo cluster

--- mongo.js
+++ mongo.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose')
-const {mongoPath} = require('./config.json')
+//const {mongoPath} = require('./config.json')
+
+const mongoPath = 'mongodb+srv://Mint:C3BCYY2ZShrkRgJW@realmcluster.smdnk.mongodb.net/DeferenceBot?retryWrites=true&w=majority'
 
 module.exports = async () => {
     await mongoose.connect(mongoPath, {
