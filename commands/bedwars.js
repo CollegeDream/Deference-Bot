7217f86f990df1ae8e09858e4249aa74e48544bc
Minh Nguyen <mnguyen24@my.whitworth.edu>
2021-03-28 15:53:19 -0700

    update bedwars.js

--- commands/bedwars.js
+++ commands/bedwars.js
@@ -1,9 +1,10 @@
-const mongo = require('../mongo')
+
 const Discord = require('discord.js')
 const fetch = require('node-fetch')
 const fs = require('fs')
 const config = require('../config.json')
-const getUUID = require('../Schemas/saveUUID')
+const saveUUID = require('../Schemas/saveUUID')
+const mongo = require('../mongo')
 const { get } = require('mongoose')
 
 
@@ -13,13 +14,51 @@ module.exports = {
     description: 'get player\'s Bedwars stats',
     cooldown: 6,
     async execute(message, args){
-        var uuid = '9a3e0e10010d4867ae976982d7d60609'
-        message.channel.send('Command will be coming soon!')
-        //fetch UUID from database, if UUID doesn't exist ask the user to enter IGN
-
-        //get the player object from API
+        var uuid;
         
-        async function getPlayer(uuid){
+        //fetch UUID from database, if UUID doesn't exist ask the user to enter IGN
+        await mongo().then(async (mongoose) => {
+            try{
+                author = await saveUUID.findOne({_id: message.author.id}, (err)=>{
+                    if(err){
+                        message.channel.send('Error occured: ' + err)
+                    }
+                })
+                if(author){
+                    uuid = author.uuid;
+                }
+            } finally {
+                mongoose.connection.close()
+            }
+        });
+
+        (async function retrivePlayer(uuid){
+            if(args[0]){
+                let player = await getPlayerWithName(args[0]);
+                if (!player){
+                    message.channel.send('This player does not exist!');
+                } else {
+                    getBedwarsStats(player);
+                }
+            } else {
+                if(uuid){
+                    let player = await getPlayerWithID(uuid);
+                    getBedwarsStats(player);
+                } else {
+                    message.channel.send('Provide an IGN, or use \`&register\` to use \`&bw\` with no arguments')
+                }
+            }
+        })(uuid);
+        //get the player object from API
+        function getUUID(username) {
+            return fetch(`https://api.mojang.com/users/profiles/minecraft/${username}`)
+            .then(data => data.json())
+            .then((player) => {
+              return player.id
+            }).catch(e=>null);
+        }
+        
+        async function getPlayerWithID(uuid){
             return fetch(`https://api.hypixel.net/player?uuid=${uuid}&key=${config.apiKey}`)
             .then(response => response.json())
             .then(({player})=>{
@@ -27,17 +66,24 @@ module.exports = {
             }).catch(e=>console.log(e));
         }
 
+        async function getPlayerWithName(username){
+            const id = await getUUID(username)
+            return fetch(`https://api.hypixel.net/player?uuid=${id}&key=${config.apiKey}`)
+            .then(result => result.json())
+            .then(({player}) => {
+            return player
+        }).catch(e=>null);
+        };
         //useful constants
-        const player = await getPlayer(uuid);
-        const username = player.displayname;
-        const {Bedwars} = player.stats;
+
 
         //create an embed object here
-        (async function (player){
-        const bedwars_stats = new Discord.MessageEmbed()
+        async function getBedwarsStats(player){
+            const username = player.displayname;
+            const {Bedwars} = player.stats;
+            const bedwars_stats = new Discord.MessageEmbed()
             .setColor('#e82e20')
-            .setAuthor(`Player → ${player.displayname} → Bedwars`)
-            .setThumbnail(`https://crafatar.com/avatars/${player.uuid}`)
+            .setAuthor(`Player → ${player.displayname} → Bedwars`, `https://crafatar.com/avatars/${player.uuid}`)
             .setTitle(`(${player.achievements.bedwars_level}★) ${username}`)
             .setURL(`https://plancke.io/hypixel/player/stats/${username}`)
             //add fields to the embed object
@@ -54,8 +100,48 @@ module.exports = {
             bedwars_stats.addField('\*\*Items collected\*\*', `${resource_array.join('\n')}`, true)
             bedwars_stats.addField('\*\*Winstreak\*\*', Bedwars.winstreak, true)
             bedwars_stats.addField('\*\*Items purchased\*\*', Bedwars.items_purchased_bedwars, true)
+            bedwars_stats.addField('\u200B', '\u200B', false)
+
+            let kills_deaths = {
+                'Overall': `${Bedwars.kills_bedwars} | ${Bedwars.deaths_bedwars}`,
+                'Solo': `${Bedwars.eight_one_kills_bedwars} | ${Bedwars.eight_one_deaths_bedwars}`,
+                'Double': `${Bedwars.eight_two_kills_bedwars} | ${Bedwars.eight_two_deaths_bedwars}`,
+                '3v3v3v3':  `${Bedwars.four_three_kills_bedwars} | ${Bedwars.four_three_deaths_bedwars}`,
+                '4v4v4v4': `${Bedwars.four_four_kills_bedwars} | ${Bedwars.four_four_deaths_bedwars}`,
+            }
+            let kills_deaths_array = [];
+            for(mode in kills_deaths){
+                kills_deaths_array.push(`\*\*${mode}:\*\* ${kills_deaths[mode]}`)
+            }
+            bedwars_stats.addField('\*\*Kills | Deaths\*\*', kills_deaths_array.join('\n'), true)
+            
+            let finals = {
+                'Overall': `${Bedwars.final_kills_bedwars} | ${Bedwars.final_deaths_bedwars}`,
+                'Solo': `${Bedwars.eight_one_final_kills_bedwars} | ${Bedwars.eight_one_final_deaths_bedwars}`,
+                'Double': `${Bedwars.eight_two_final_kills_bedwars} | ${Bedwars.eight_two_final_deaths_bedwars}`,
+                '3v3v3v3': `${Bedwars.four_three_final_kills_bedwars} | ${Bedwars.four_three_final_deaths_bedwars}`,
+                '4v4v4v4': `${Bedwars.four_four_final_kills_bedwars} | ${Bedwars.four_four_final_deaths_bedwars}`,
+            }
+            let final_array = []
+            for(mode in finals){
+                final_array.push(`\*\*${mode}:\*\* ${finals[mode]}`)
+            }
+            bedwars_stats.addField(`\*\*Final K|D\*\*`, final_array.join('\n'), true)
+
+            let fkd_ratios = {
+                'Overall': Bedwars.final_kills_bedwars / Bedwars.final_deaths_bedwars,
+                'Solo': Bedwars.eight_one_final_kills_bedwars / Bedwars.eight_one_final_deaths_bedwars,
+                'Double': Bedwars.eight_two_final_kills_bedwars / Bedwars.eight_two_final_deaths_bedwars,
+                '3v3v3v3': Bedwars.four_three_final_kills_bedwars / Bedwars.four_three_final_deaths_bedwars,
+                '4v4v4v4': Bedwars.four_four_final_kills_bedwars / Bedwars.four_four_final_deaths_bedwars,
+            }
+            let fkdr_array = []
+            for(mode in fkd_ratios){
+                fkdr_array.push(`\*\*${mode}:\*\* ${fkd_ratios[mode].toFixed(2)}`)
+            }
+            bedwars_stats.addField('\*\*FKDR\*\*', fkdr_array.join('\n'), true)
             message.channel.send(bedwars_stats);
-        })(player);
+        };
         
         async function getBedWarsPrestige(level){
             // eslint-disable-next-line max-len
