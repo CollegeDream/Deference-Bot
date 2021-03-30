44263f02a1de3a954b6d90f469da53c445e6f2f5
Minh Nguyen <mnguyen24@my.whitworth.edu>
2021-03-30 00:27:25 -0700

    fnished bedwars.js

--- commands/bedwars.js
+++ commands/bedwars.js
@@ -108,16 +108,26 @@ module.exports = {
             bedwars_stats.addField('\*\*Items purchased\*\*', Bedwars.items_purchased_bedwars, true)
             bedwars_stats.addField('\u200B', '\u200B', false)
 
-            let kills_deaths = {
-                'Overall': `${Bedwars.kills_bedwars} | ${Bedwars.deaths_bedwars}`,
-                'Solo': `${Bedwars.eight_one_kills_bedwars} | ${Bedwars.eight_one_deaths_bedwars}`,
-                'Double': `${Bedwars.eight_two_kills_bedwars} | ${Bedwars.eight_two_deaths_bedwars}`,
-                '3v3v3v3':  `${Bedwars.four_three_kills_bedwars} | ${Bedwars.four_three_deaths_bedwars}`,
-                '4v4v4v4': `${Bedwars.four_four_kills_bedwars} | ${Bedwars.four_four_deaths_bedwars}`,
-            }
+            let kills = [
+                {'Overall': Bedwars.kills_bedwars || 0},
+                {'Solo': Bedwars.eight_one_kills_bedwars || 0},
+                {'Double': Bedwars.eight_two_kills_bedwars || 0},
+                {'3v3v3v3': Bedwars.four_three_kills_bedwars || 0},
+                {'4v4v4v4': Bedwars.four_four_kills_bedwars || 0},
+            ]
+
+            let deaths = [
+                {'Overall': Bedwars.deaths_bedwars || 0},
+                {'Solo': Bedwars.eight_one_deaths_bedwars || 0},
+                {'Double': Bedwars.eight_two_deaths_bedwars || 0},
+                {'3v3v3v3': Bedwars.four_three_deaths_bedwars || 0},
+                {'4v4v4v4': Bedwars.four_four_deaths_bedwars || 0},
+            ]
             let kills_deaths_array = [];
-            for(mode in kills_deaths){
-                kills_deaths_array.push(`\*\*${mode}:\*\* ${kills_deaths[mode]}`)
+            for(let i = 0; i < kills.length; i++){
+                for(let mode in kills[i]){
+                    kills_deaths_array.push(`${mode}: ${kills[i][mode]} | ${deaths[i][mode]}`)
+                }
             }
             bedwars_stats.addField('\*\*Kills | Deaths\*\*', kills_deaths_array.join('\n'), true)
             
@@ -141,16 +151,77 @@ module.exports = {
             let fkdr = [];
             for(let i = 0; i < final_kills.length; i++){
                 for(let mode in final_kills[i]){
-                    let ratio = ((final_kills[i][mode])/(final_deaths[i][mode])).toFixed(2);
+                    let ratio = ((final_kills[i][mode])/(final_deaths[i][mode] || 1)).toFixed(2);
                     final_array.push(`${mode}: ${final_kills[i][mode]} | ${final_deaths[i][mode]}`)
                     fkdr.push(`${mode}: ${ratio}`)
                 }
             }
             
-            bedwars_stats.addField(`\*\*Finals\*\*`, final_array.join('\n'), true)
+            bedwars_stats.addField(`\*\*Finals K|D\*\*`, final_array.join('\n'), true)
             bedwars_stats.addField(`\*\*FKDR\*\*`, fkdr.join('\n'), true)
 
-    
+            let wins = [
+                {'Overall': Bedwars.wins_bedwars || 0},
+                {'Solo': Bedwars.eight_one_wins_bedwars || 0},
+                {'Double': Bedwars.eight_two_wins_bedwars || 0},
+                {'3v3v3v3': Bedwars.four_three_wins_bedwars || 0},
+                {'4v4v4v4': Bedwars.four_four_wins_bedwars || 0},
+            ]
+
+            let losses = [
+                {'Overall': Bedwars.losses_bedwars || 0},
+                {'Solo': Bedwars.eight_one_losses_bedwars || 0},
+                {'Double': Bedwars.eight_two_losses_bedwars || 0},
+                {'3v3v3v3': Bedwars.four_three_losses_bedwars || 0},
+                {'4v4v4v4': Bedwars.four_four_losses_bedwars || 0},
+            ]
+            
+            let win_loss_array = [];
+            let win_loss_ratio = [];
+
+            for(let i = 0; i < wins.length; i++){
+                for(let mode in wins[i]){
+                    let ratio = ((wins[i][mode])/(losses[i][mode] || 1)).toFixed(2);
+                    win_loss_array.push(`${mode}: ${wins[i][mode]} | ${losses[i][mode]}`)
+                    win_loss_ratio.push(`${mode}: ${ratio}`)
+                }
+            }
+            bedwars_stats.addField('\*\*Wins | Losses\*\*', win_loss_array.join('\n'), true)
+            bedwars_stats.addField('\*\*W | L Ratio\*\*', win_loss_ratio.join('\n'), true)
+            bedwars_stats.addField('\u200B', '\u200B', true)
+            
+            
+            let beds_broken = [
+                {'Overall': Bedwars.beds_broken_bedwars || 0},
+                {'Solo': Bedwars.eight_one_beds_broken_bedwars || 0},
+                {'Double': Bedwars.eight_two_beds_broken_bedwars || 0},
+                {'3v3v3v3': Bedwars.four_three_beds_broken_bedwars || 0},
+                {'4v4v4v4': Bedwars.four_four_beds_broken_bedwars || 0},
+            ]
+
+            let beds_lost = [
+                {'Overall': Bedwars.beds_lost_bedwars || 0},
+                {'Solo': Bedwars.eight_one_beds_lost_bedwars || 0},
+                {'Double': Bedwars.eight_two_beds_lost_bedwars || 0},
+                {'3v3v3v3': Bedwars.four_three_beds_lost_bedwars || 0},
+                {'4v4v4v4': Bedwars.four_four_beds_lost_bedwars || 0},
+            ]
+
+            let beds_broken_lost = [];
+            let beds_ratio = [];
+
+            for(let i = 0; i < beds_broken.length; i++){
+                for(let mode in beds_broken[i]){
+                    let ratio = ((beds_broken[i][mode])/(beds_lost[i][mode] || 1)).toFixed(2);
+                    beds_broken_lost.push(`${mode}: ${beds_broken[i][mode]} | ${beds_lost[i][mode]}`)
+                    beds_ratio.push(`${mode}: ${ratio}`)
+                }
+            }
+
+            bedwars_stats.addField('\*\*Beds B | L\*\*', beds_broken_lost.join('\n'), true)
+            bedwars_stats.addField('\*\*B | L Ratio\*\*', beds_ratio.join('\n'), true)
+            bedwars_stats.addField('\u200B', '\u200B', true)
+
             message.channel.send(bedwars_stats);
         };
         
